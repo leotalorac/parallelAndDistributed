f663b6fec0fe3c49bda8602fafb0f5fc1744f011
leotalorac <leotalorac@unal.edu.co>
2020-10-08 08:31:48 -0500

    reading

--- practice1/graphics.py
+++ practice1/graphics.py
@@ -4,8 +4,9 @@ import matplotlib
 f = open("results.txt", "r")
 # 1 2 4 8 16 threads
 # 720p 1080p 4k
-for i in range(3):
-    for j in range(5):
+for i in range(1):
+    for j in range(1):
         for k in range(10):
             f.read()
         avg = float(f.read())
+        print(avg)
--- practice1/run.sh
+++ practice1/run.sh
@@ -1,4 +1,18 @@
 echo "compiling..."
 g++ reduction.cpp -std=c++11 `pkg-config --cflags --libs opencv` -lpthread
 echo "runing..."
-./a.out "./imgs/img4k.jpg" "./hola.jpg" 8 > results.txt
+./a.out "./imgs/img4k.jpg" "./hola.jpg" 1 
+./a.out "./imgs/img4k.jpg" "./hola.jpg" 2 
+./a.out "./imgs/img4k.jpg" "./hola.jpg" 4 
+./a.out "./imgs/img4k.jpg" "./hola.jpg" 8 
+./a.out "./imgs/img4k.jpg" "./hola.jpg" 16 
+./a.out "./imgs/img1080.jpg" "./hola.jpg" 1 
+./a.out "./imgs/img1080.jpg" "./hola.jpg" 2 
+./a.out "./imgs/img1080.jpg" "./hola.jpg" 4 
+./a.out "./imgs/img1080.jpg" "./hola.jpg" 8 
+./a.out "./imgs/img1080.jpg" "./hola.jpg" 16 
+./a.out "./imgs/img720.jpg" "./hola.jpg" 1 
+./a.out "./imgs/img720.jpg" "./hola.jpg" 2 
+./a.out "./imgs/img720.jpg" "./hola.jpg" 4 
+./a.out "./imgs/img720.jpg" "./hola.jpg" 8 
+./a.out "./imgs/img720.jpg" "./hola.jpg" 16 
