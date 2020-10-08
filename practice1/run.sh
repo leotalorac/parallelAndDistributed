2ebee6e7727ad433a1586ddc5a820230049361c7
leotalorac <leotalorac@unal.edu.co>
2020-10-08 02:26:33 -0500

    fixed and started secuential program

--- practice1/run.sh
+++ practice1/run.sh
@@ -1,4 +1,4 @@
 echo "compiling..."
 g++ reduction.cpp -std=c++11 `pkg-config --cflags --libs opencv` -lpthread
 echo "runing..."
-./a.out "./imgs/img720.jpg" "./hola.jpg" 1
+./a.out "./imgs/image1_1080p.jpg" "./hola.jpg" 1
