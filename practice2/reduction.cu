b6b88bf94c54e03609bf5de5b940282f0f640353
Juan Camilo <jcgomezlo@unal.edu.co>
2020-11-26 13:14:42 -0500

    ASDAD

--- practice2/reduction.cu
+++ practice2/reduction.cu
@@ -81,7 +81,7 @@ int main(int argc, char* argv[]) {
 
    
     const dim3 threadsPerBlock(threads, threads);
-    const dim3 numBlocks(outImg.cols / threadsPerBlock.x, outImg.rows / threadsPerBlock.y);
+    const dim3 numBlocks(WIDTH / threadsPerBlock.x, HEIGHT / threadsPerBlock.y);
     for(int i = 0; i < ITERATIONS; i++){
         nearest_neighbour_scaling<<<numBlocks, threadsPerBlock>>>(input_image_pointer, output_image_pointer, originalImg.size().width, originalImg.size().height);
     }
